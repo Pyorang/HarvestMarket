4cf3042c1f7460e715cdf34d271563a7d19d8a59
이승빈 <144720218+Pyorang@users.noreply.github.com>
2026-01-26 15:39:43 +0900

    feat : 타겟 지정 기능 만들기

---
+++ Assets/02. Scripts/InGame/ClickTarget.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RaycastPointerHandler : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector3 touchPosition = eventData.position;
+
+        Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("맞은 물체: " + hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.Log("공중에 터치함");
+        }
+    }
+}
---
+++ Assets/02. Scripts/InGame/Farm.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Farm : MonoBehaviour
+{
+    private GameObject[] _animals;
+
+
+}
