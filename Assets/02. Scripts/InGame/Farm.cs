8241d2bbd9a3d35ce7ddb337a20d4debc68d6318
이승빈 <144720218+Pyorang@users.noreply.github.com>
2026-01-26 17:03:10 +0900

    feat : 동물 애니메이션 스크립트 추가

--- Assets/02. Scripts/InGame/Farm.cs
+++ Assets/02. Scripts/InGame/Farm.cs
@@ -4,7 +4,9 @@ public class Farm : MonoBehaviour
 {
     // NOTE: 농장에서 키우는 동물의 종류를 선택하고, 늘리거나 줄일 수 있게 하는 역할.
 
-    private Animal _animalType;
+    [SerializeField] private Animal _animalType;
 
-    private GameObject[] _animals;
+    [SerializeField] private GameObject[] _animals;
+
+
 }
