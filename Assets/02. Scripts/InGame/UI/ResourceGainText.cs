c656c9a85420d484c70f52e977c2dd42a05029eb
Lee Seung Been <144720218+Pyorang@users.noreply.github.com>
2026-01-27 01:48:18 +0900

    feat : 올라가는 효과 만들기

---
+++ Assets/02. Scripts/InGame/UI/ResourceGainText.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class ResourceGainText : MonoBehaviour
+{
+    [SerializeField] private float _moveDistance = 0.1f;
+    [SerializeField] private float _duration = 1f;
+
+    private void OnEnable()
+    {
+        Play();
+    }
+
+    private void Play()
+    {
+        transform.DOLocalMoveY(transform.localPosition.y + _moveDistance, _duration)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() => gameObject.SetActive(false));
+    }
+
+    private void OnDisable()
+    {
+        transform.DOKill();
+    }
+}
